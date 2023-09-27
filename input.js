e517b8565dc399ed28d0cdeb90f1131bc6f5b14f
JP Saliba <jpsaliba@outlook.com>
2023-09-27 15:11:55 -0400

    Added movement tracking to input, and changed play to support the changes

---
+++ input.js
@@ -0,0 +1,31 @@
+let connection;
+
+const handleUserInput = function (data) {
+  if (data === 'w') {
+    connection.write("Move: up");
+  } else if (data === 'a') {
+    connection.write("Move: left");
+  } else if (data === 's') {
+    connection.write("Move: down");
+  } else if (data === 'd') {
+    connection.write("Move: right");
+  } else if (data === '\u0003') {
+    process.exit();
+  }
+}
+
+const setupInput = (conn) => {
+  connection = conn;
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+
+  stdin.on("data", handleUserInput);
+
+  return stdin;
+}
+
+module.exports = {
+  setupInput
+}

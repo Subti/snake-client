a03941574ae0242ed980cb299f92642f225f9edf
JP Saliba <jpsaliba@outlook.com>
2023-09-27 12:59:56 -0400

    Added client.js to handle connections to the server and exported

--- play.js
+++ play.js
@@ -1,21 +1,4 @@
-const net = require("net");
-
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: "localhost",
-    port: 50541,
-  });
-
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-
-  conn.on("connect", (data) => {
-    console.log(data)
-  });
-
-  return conn;
-};
+const client = require("./client");
 
 console.log("Connecting ...");
-connect();
+client.connect();
